3f19713ad6997439c355960655a9dc8ae7f873aa
Alex Vogel <avogelm@gmail.com>
2017-10-06 15:07:22 +0200

    Script botones de destino + ciudadano

---
+++ project/Assets/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Citizen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void sendToHappyDoor(){
+		Debug.Log("oh sweet");
+	}
+	public void sendToHappifier(){
+		Debug.Log("fuck my life");
+	}
+}
---
+++ project/Assets/DoorButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseOver(){
+		if(Input.GetMouseButtonDown(0)){
+			Debug.Log("PULSO");
+			sendCitizenToDoor();
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			Debug.Log("SUELTO");
+		}
+	}
+
+	void sendCitizenToDoor(){
+		GameObject ctzn = GameObject.Find("citizen");
+		Citizen ctzscrpt = (Citizen) ctzn.GetComponent(typeof(Citizen));
+
+		if(this.gameObject.transform.name == "Happy"){
+			ctzscrpt.sendToHappyDoor();
+		}else{
+			ctzscrpt.sendToHappifier();
+		}
+
+	}
+}
